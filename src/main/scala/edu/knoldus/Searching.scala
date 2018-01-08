195949edaa3c4f055f52b5a9796e237299fac3e9
ThakurPriyanka <aroma.priyanka@gmail.com>
2018-01-09 00:08:42 +0530

    Initial commit

---
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -0,0 +1,43 @@
+package edu.knoldus
+
+class Searching {
+  def binarySearchMine(array: Array[Int], elem: Int, start: Int = 0, end: Int): Int = {
+    if (start > end) -1
+    val mid = start + (end - start + 1) / 2
+    if(mid > array.length) -1
+    if (array(mid) == elem) {
+      mid
+    }
+    else if (array(mid) > elem) {
+      binarySearchMine(array, elem, start, mid - 1)
+    }
+    else {
+      binarySearchMine(array, elem, mid + 1, end)
+    }
+  }
+  def binarySearch(array: Array[Int], elem: Int): Boolean = {
+
+    val output = binarySearchMine(array, elem, 0, array.length)
+    if (output == -1) {
+      false
+    }
+    else {
+      true
+    }
+  }
+
+  def linearSearch(array: Array[Int], elem: Int): Boolean = {
+    var index = -1
+    for (item <- 0 to (array.length - 1)) {
+      if (array(item) == elem) {
+        index = item
+      }
+    }
+    if (index != -1) {
+      true
+    }
+    else {
+      false
+    }
+  }
+}
---
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -0,0 +1,50 @@
+package edu.knoldus
+
+class Sorting {
+
+  def insertionSort(array: Array[Int]): Array[Int] = {
+    for(i <- 1 until array.length )
+    {
+      val key = array(i)
+      var j = i-1
+
+      while( j>=0 && array(j)>key)
+      {
+        array(j + 1) = array(j)
+        j = j-1
+      }
+      array(j + 1) = key
+    }
+    array
+  }
+
+  def selectionSort(array: Array[Int]): Array[Int] = {
+    for(i <- 0 to array.length - 1){
+      var min: Int = i
+      for(j <- i + 1 to array.length - 1 ){
+        if(array(min) > array(j) ) {
+          min = j
+        }
+      }
+      val temp = array(i)
+      array(i)= array(min)
+      array(min)= temp
+    }
+    array
+  }
+
+  def bubbleSort(array: Array[Int]): Array[Int] = {
+    for(i <- 1 to array.length - 1){
+      for(j <- i - 1 to 0 by - 1 ){
+        if(array(j) > array(j + 1) ){
+
+          val temp = array(j)
+          array(j)= array(j + 1)
+          array(j + 1) = temp
+        }
+      }
+    }
+    array
+  }
+
+}
