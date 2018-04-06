d4e82e4446021c25d42cb61f86ac783c0f31b747
Manjot Kaur <manjotmona@gmail.com>
2018-04-06 12:13:32 +0530

    trying git

--- src/main/scala/edu/knoldus/Sorting.scala
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -2,21 +2,23 @@ package edu.knoldus
 
 class Sorting {
 
-  def insertionSort(array: Array[Int]): Array[Int] = {
-    for(i <- 1 until array.length )
-    {
-      val key = array(i)
-      var j = i-1
+  // Git-Learning changes by Manjot.
 
-      while( j>=0 && array(j)>key)
-      {
-        array(j + 1) = array(j)
-        j = j-1
-      }
-      array(j + 1) = key
-    }
-    array
-  }
+//  def insertionSort(array: Array[Int]): Array[Int] = {
+//    for(i <- 1 until array.length )
+//    {
+//      val key = array(i)
+//      var j = i-1
+//
+//      while( j>=0 && array(j)>key)
+//      {
+//        array(j + 1) = array(j)
+//        j = j-1
+//      }
+//      array(j + 1) = key
+//    }
+//    array
+//  }
 
   def selectionSort(array: Array[Int]): Array[Int] = {
     for(i <- 0 to array.length - 1){
